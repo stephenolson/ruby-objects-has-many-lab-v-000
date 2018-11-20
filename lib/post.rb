a621d1bdfd79624faf4b43dc68f0f2005beff42b
Stephen Olson <stephenolson@gmail.com>
2018-11-20 18:14:47 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
 class Post
   attr_accessor :title, :author
-  @all = [ ]
+  @@all = [ ]
  
   def initialize(title)
     @title = title
-    @@all
+    @@all << self
   end
   
   def author_name
