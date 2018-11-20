85a0b865b56a4a4ebc53c9a14e5a4ae1eae063f0
Stephen Olson <stephenolson@gmail.com>
2018-11-20 18:02:34 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author
+  
+  attr_accessor :name, :song
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name
+    @songs = [ ]
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def song_count
+    @@song_count
+  end
+  
+end
