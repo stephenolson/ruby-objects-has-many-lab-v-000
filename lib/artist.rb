bcf9d2f20740e3b5627f347f83ddf2d781e70391
Stephen Olson <stephenolson@gmail.com>
2018-11-20 17:50:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,14 +7,16 @@ class Artist
     @songs = [ ]
   end
   
+  def songs
+    @songs
+  end
+  
   def add_song(song)
     @songs << song
     song.artist = self
   end
   
-  def songs
-    @songs
-  end
+  
   
   def add_song_by_name(name)
     song = Song.new(name)
